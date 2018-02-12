0c88e5520168beb9abfc00bb9be47874f54015d3
Patrick Maciel <patrickmaciel.info@gmail.com>
2018-02-12 18:14:05 -0200

    Estudando Strings

---
+++ playground.rb
@@ -0,0 +1,17 @@
+puts 'Patrick' + ' ' + 'Maciel'
+
+firstname = 'Patrick'
+lastname = 'Maciel'
+
+puts firstname + ' ' + lastname
+puts "#{firstname} #{lastname}"
+
+puts 'Opa e ai \'single quotes\''
+puts "Opa e ai 'single quotes'"
+
+puts firstname.length
+
+puts 'Digite seu nome:'
+nome = gets.chomp
+
+puts "Seu nome #{nome}"
